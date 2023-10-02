2ea23eda2c1dedea081df9e77fa61f394cabf994
Jacob Erisman <jacob.erisman@gmail.com>
2023-10-02 16:33:17 -0400

    add data to JSON file

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
-document.addEventListener("DOMContentLoaded", () => console.log("File Connected"))
+document.addEventListener("DOMContentLoaded", function(){
+    fetch('http://localhost:3000/colorado14ers')
+        .then(response => response.json())
+        .then(data => {
+            //Add data from JSON file to table
+            const infoTable = document.getElementById('infoTable');
+            const tableBody = document.createElement('tbody');
+
+            data.forEach(peak => {
+                const row = document.createElement('tr');
+
+                const elevationRankCell = document.createElement('td');
+                elevationRankCell.textContent = peak.elevationRank;
+                row.appendChild(elevationRankCell);
+
+                const nameCell = document.createElement('td');
+                nameCell.textContent = peak.name;
+                row.appendChild(nameCell);
+
+                const elevationCell = document.createElement('td');
+                elevationCell.textContent = peak.elevation;
+                row.appendChild(elevationCell);
+
+                const rangeCell = document.createElement('td');
+                rangeCell.textContent = peak.range;
+                row.appendChild(rangeCell);
+
+                const townCell = document.createElement('td');
+                townCell.textContent = peak.nearestTown;
+                row.appendChild(townCell);
+
+                const climbedCell = document.createElement('td');
+                const checkbox = document.createElement('input');
+                checkbox.type = 'checkbox';
+                climbedCell.appendChild(checkbox);
+                row.appendChild(climbedCell);
+
+                tableBody.appendChild(row)            
+            });
+            infoTable.appendChild(tableBody);
+            
+            //Add event listener for sorting table by dropdown menu selection
+            const sortSelect = document.getElementById('sortSelect');
+            sortSelect.addEventListener('change', function(){
+                const sortBy = sortSelect.value;
+
+            })
+        });
+});
