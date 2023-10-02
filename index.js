81f3c4fecf626459a3c7c6ca01d6819581279384
Jacob Erisman <jacob.erisman@gmail.com>
2023-10-02 19:07:57 -0400

    add sorting feature

--- index.js
+++ index.js
@@ -2,63 +2,73 @@ document.addEventListener("DOMContentLoaded", function(){
     fetch('http://localhost:3000/colorado14ers')
         .then(response => response.json())
         .then(data => {
-            //Add data from JSON file to table
             const infoTable = document.getElementById('infoTable');
             const tableBody = document.createElement('tbody');
 
-            data.forEach(peak => {
-                const row = document.createElement('tr');
+            //Create populateData function to populate table with JSON data
+            console.log(data)
+            const populateTable = function(data) {
+                while (tableBody.firstChild) {
+                    tableBody.firstChild.remove();
+                }
 
-                const elevationRankCell = document.createElement('td');
-                elevationRankCell.textContent = peak.elevationRank;
-                row.appendChild(elevationRankCell);
+                data.forEach(peak => {
+                    const row = document.createElement('tr');
 
-                const nameCell = document.createElement('td');
-                nameCell.textContent = peak.name;
-                row.appendChild(nameCell);
+                    const elevationRankCell = document.createElement('td');
+                    elevationRankCell.textContent = peak.elevationRank;
+                    row.appendChild(elevationRankCell);
 
-                const elevationCell = document.createElement('td');
-                elevationCell.textContent = peak.elevation;
-                row.appendChild(elevationCell);
+                    const nameCell = document.createElement('td');
+                    nameCell.textContent = peak.name;
+                    row.appendChild(nameCell);
 
-                const rangeCell = document.createElement('td');
-                rangeCell.textContent = peak.range;
-                row.appendChild(rangeCell);
+                    const elevationCell = document.createElement('td');
+                    elevationCell.textContent = peak.elevation;
+                    row.appendChild(elevationCell);
 
-                const townCell = document.createElement('td');
-                townCell.textContent = peak.nearestTown;
-                row.appendChild(townCell);
+                    const rangeCell = document.createElement('td');
+                    rangeCell.textContent = peak.range;
+                    row.appendChild(rangeCell);
 
-                const climbedCell = document.createElement('td');
-                const checkbox = document.createElement('input');
-                checkbox.type = 'checkbox';
-                climbedCell.appendChild(checkbox);
-                row.appendChild(climbedCell);
+                    const townCell = document.createElement('td');
+                    townCell.textContent = peak.nearestTown;
+                    row.appendChild(townCell);
 
-                tableBody.appendChild(row)            
-            });
-            infoTable.appendChild(tableBody);
-            
+                    const climbedCell = document.createElement('td');
+                    const checkbox = document.createElement('input');
+                    checkbox.type = 'checkbox';
+                    climbedCell.appendChild(checkbox);
+                    row.appendChild(climbedCell);
+
+                    tableBody.appendChild(row)            
+                });
+
+                infoTable.appendChild(tableBody);
+            };
+            populateTable(data);
             
             //Add event listener for sorting table by dropdown menu selection
             const sortSelect = document.getElementById('sortSelect');
             sortSelect.addEventListener('change', function(){
                 const sortBy = sortSelect.value;
 
-                data.sort((a, b) => {
-                    if (a[sortBy] < b[sortBy]) {
-                        return -1;
-                    }
-                    if (a[sortBy] > b[sortBy]) {
-                        return 1;
-                    }
-                    return 0;
-                });
-
-                while (tableBody.firstChild){
-
+                if (sortBy === "elevationDescending") {
+                    data.sort((a, b) => parseInt(b.elevation.replace(/,/g, "")) - parseInt(a.elevation.replace(/,/, "")));
+                } else if (sortBy === "elevationAscending") {
+                    data.sort((a,b) => parseInt(a.elevation.replace(/,/g, "")) - parseInt(b.elevation.replace(/,/g, "")));
                 }
 
-            })
+                    data.sort((a, b) => {
+                        if (a[sortBy] < b[sortBy]) {
+                            return -1;
+                        }
+                        if (a[sortBy] > b[sortBy]) {
+                            return 1;
+                        }
+                        return 0;
+                    });
+                populateTable(data); 
+            });    
         });
-})
+});
